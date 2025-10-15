0bc820ce20cf33604145515a604319acaa48bdae
Cori Graves <corigraves97@gmail.com>
2025-10-15 17:51:59 -0400

    css, about page

--- petcollector/pet_app/views.py
+++ petcollector/pet_app/views.py
@@ -4,4 +4,7 @@ from django.http import HttpResponse
 def home(request):
     return HttpResponse('<h1> Welcome to the Pet Collector </h1>')
 
+def about(request):
+    return render(request, 'about.html')
+
 # Create your views here.
--- petcollector/petcollector/urls.py
+++ petcollector/petcollector/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('pet_app.urls'))
+    path('', include('pet_app.urls')),
+    path('about/', include('pet_app.urls'))
 ]
